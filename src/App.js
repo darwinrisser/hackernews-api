c9fe5b871be2701e3b6e0bef48704459e104e17d
Darwin Risser <riser.darwin@gmail.com>
2019-04-25 09:14:54 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    reddit: []
+  }
+
+  componentDidMount(){
+    let uri = 'https://www.reddit.com/r/all.json'
+    fetch(uri)
+      .then(function(data){
+        return data.json();
+        })
+      .then(function(data){
+        return (this.setState({reddit:data.data.children.map(child => child.data)}));
+        })
+  }
+
+  render() {
+    return (
+      <div>
+        This is a test
+      </div>
+    );
+  }
 }
 
 export default App;
